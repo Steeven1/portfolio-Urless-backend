80ccaa91cf6a466b325387560f296dbcf1967ed7
Steeven1 <alfredsteveen.456@gmail.com>
2025-06-12 14:39:16 -0500

    fix: format

--- src/main/java/portfolio_urless_backend/url/infra/UrlController.java
+++ src/main/java/portfolio_urless_backend/url/infra/UrlController.java
@@ -11,33 +11,35 @@ import portfolio_urless_backend.url.domain.entities.Url;
 
 import java.util.HashMap;
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/urls")
 public class UrlController {
 
-    private final FindAllUrl findAllUrlUseCase;
-    private final ShortenUrl shortenUrlUseCase;
-    private final FindByUrl  findByUrlUseCase;
-    public UrlController(FindAllUrl findAllUrlUseCase,
-                         ShortenUrl shortenUrlUseCase,
-                         FindByUrl  findByUrlUseCase
-                         ){
-        this.findAllUrlUseCase = findAllUrlUseCase;
-        this.shortenUrlUseCase = shortenUrlUseCase;
-        this.findByUrlUseCase  = findByUrlUseCase;
-    }
+  private final FindAllUrl findAllUrlUseCase;
+  private final ShortenUrl shortenUrlUseCase;
+  private final FindByUrl  findByUrlUseCase;
+  public UrlController(
+    FindAllUrl findAllUrlUseCase,
+    ShortenUrl shortenUrlUseCase,
+    FindByUrl  findByUrlUseCase
+  )
+  {
+    this.findAllUrlUseCase = findAllUrlUseCase;
+    this.shortenUrlUseCase = shortenUrlUseCase;
+    this.findByUrlUseCase  = findByUrlUseCase;
+  }
 
-    @GetMapping
-    public List<Url> findAll(){
-        return this.findAllUrlUseCase.run();
-    }
+  @GetMapping
+  public List<Url> findAll(){
+    return this.findAllUrlUseCase.run();
+  }
 
   @GetMapping("/{short_url}")
   public ResponseEntity<?> find(
     @PathVariable("short_url") String short_url
-  ){
+  )
+  {
 
     return ResponseEntity.status(HttpStatus.FOUND)
       .header("Location",
@@ -51,33 +53,34 @@ public class UrlController {
       .build();
   }
 
-    @PostMapping
-    public ResponseEntity<?> shortenUrl(
-      @RequestBody(required = false)
-        final UrlDto url_req
-
-    ) {
-
-      UrlDto url_saved =  this.shortenUrlUseCase.run(
-        url_req
-      );
-
-      HashMap<String, String> errors = url_saved.getErrors();
-
-      if( !url_saved.getErrors().isEmpty()  ){
-
-        return ResponseEntity
-          .status( HttpStatus.BAD_REQUEST )
-          .body(
-            url_saved
-          );
-      }
-
-
-        return ResponseEntity
-                .status( HttpStatus.CREATED )
-                .body(
-                  url_saved
-                );
+  @PostMapping
+  public ResponseEntity<?> shortenUrl(
+    @RequestBody(required = false)
+    final UrlDto url_req
+  ){
+    if( url_req == null ){
+      return ResponseEntity
+              .status( HttpStatus.BAD_REQUEST ).build();
     }
+    
+    UrlDto url =  this.shortenUrlUseCase.run(
+          url_req
+    );
+
+    if( !url.getErrors().isEmpty() ){
+      return ResponseEntity
+        .status(
+          HttpStatus.BAD_REQUEST
+        )
+        .body(
+          url
+        );
+    }
+
+    return ResponseEntity
+            .status( HttpStatus.CREATED )
+            .body(
+              url
+            );
+  }
 }
