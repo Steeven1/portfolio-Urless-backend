ac2089570bf34f546306a61c1926d8ad8f839237
Steeven1 <alfredsteveen.456@gmail.com>
2025-06-26 15:59:25 +0000

    fix: add cursor-based pagination using id in interface

--- src/main/java/portfolio_urless_backend/url/domain/UrlRepository.java
+++ src/main/java/portfolio_urless_backend/url/domain/UrlRepository.java
@@ -3,7 +3,7 @@ package portfolio_urless_backend.url.domain;
 import java.util.List;
 
 public interface UrlRepository<Entity> {
-    public List<Entity> findAll ();
+    public List<Entity> findAll (Long cursor);
     public void create(Entity url);
     public List<Entity> findBy(Entity url);
     public void delete(Entity url);
